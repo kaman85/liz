0b98400cff96a9a7dd6f76a5ef268985ca3a8973
kzaman85 <khosruz11@gmail.com>
2019-10-20 14:34:01 -0700

    First commit

---
+++ src/Intro.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class Intro {
+
+
+
+	public static void main(String[] args) {
+	System.out.println("what the hell");
+	for (int i = 0; i < 20; i++) {
+		System.out.println("This is i = "+ i);
+	
+		
+	}
+
+	}
+
+}
